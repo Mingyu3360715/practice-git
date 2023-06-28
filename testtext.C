9138f9a03edc76b64345cbe61963d448a44a5304
Mingyu <83645570+Mingyu3360715@users.noreply.github.com>
2023-06-28 08:52:34 +0800

    add text file

---
+++ testtext.C
@@ -0,0 +1,18 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file treeCreatorD0ToKPi.cxx
+/// \brief Writer of the 2 prong candidates in the form of flat tables to be stored in TTrees.
+///        Intended for debug or for the local optimization of analysis on small samples.
+///        In this file are defined and filled the output tables
+///
+/// \author Nicolo' Jacazio <nicolo.jacazio@cern.ch>, CERN
+/// \author Andrea Tavira García <tavira-garcia@ijclab.in2p3.fr>, IJCLab
